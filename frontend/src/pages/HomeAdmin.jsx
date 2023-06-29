638540ef1d18d3fad7eeffaf79c67588a6845ea7
Cassandra <kassy.loulou61@gmail.com>
2023-06-29 21:46:27 +0200

    Responsive de la page connection quand on arrive sur le site + celle de home admin

--- frontend/src/pages/HomeAdmin.jsx
+++ frontend/src/pages/HomeAdmin.jsx
@@ -1,13 +1,19 @@
+import { Link } from "react-router-dom";
 import NavBar from "../components/NavBar";
 import Logo from "../assets/emaus-connect.png";
 import "./HomeAdmin.css";
+import NavBarResponsive from "../components/NavBarResponsive";
 
 export default function HomeAdmin() {
   return (
     <>
       <NavBar />
       <section className="adminPageSection">
-        <section>
+        <section className="adminpage-responsive">
+          <div className="navbar-off">
+            <NavBarResponsive />
+          </div>
+
           <div className="adminpage-button-container">
             <button type="button" className="adminpage-button">
               Gérer les utilisateurs
@@ -19,6 +25,14 @@ export default function HomeAdmin() {
           <div className="testtkt">
             <img src={Logo} alt="logo" />
           </div>
+          <footer className="footer-off">
+            <Link to="/faq" className="link">
+              <li className="faq"> FAQ</li>
+            </Link>
+            <Link to="/" className="link">
+              <li className="logout">Deconnexion</li>
+            </Link>
+          </footer>
         </section>
       </section>
     </>
