594412193325156485f6ded49027ee3d630c2422
Bertrand Robert <brobert.bertrand@wanadoo.fr>
2023-06-29 09:30:09 +0200

    access control

--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -2,11 +2,17 @@ const router = require("express").Router();
 
 const userControllers = require("../controllers/userControllers");
 const passwordControllers = require("../controllers/passwordControllers");
+const authControllers = require("../controllers/authControllers");
 
-router.get("/", userControllers.browse);
+router.get("/", authControllers.isAdmin, userControllers.browse);
 router.get("/:id", userControllers.read);
 router.put("/:id", passwordControllers.hashPassword, userControllers.edit);
-router.post("/", passwordControllers.hashPassword, userControllers.add);
-router.delete("/:id", userControllers.destroy);
+router.post(
+  "/",
+  authControllers.isAdmin,
+  passwordControllers.hashPassword,
+  userControllers.add
+);
+router.delete("/:id", authControllers.isAdmin, userControllers.destroy);
 
 module.exports = router;
