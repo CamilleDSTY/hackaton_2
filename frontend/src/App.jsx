bb3e79f30720e3652025b681b934b94278be50a4
alicia coquerant <alicia.coquerant@gmail.com>
2023-06-29 09:30:35 +0200

    created a first form (CreateModelForm)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,10 @@
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
+import CreateModelForm from "./components/CreateModelForm";
 
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <Home />
-      <p>coucou</p>
-    </div>
-  );
+  return <CreateModelForm />;
 }
 
 export default App;
